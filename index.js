0d1377e805549b6fcd87c5649e874c064d6c26b9
edison cardenas <edisoncardenas151999@yahooo.com>
2022-08-26 22:04:42 -0400

    done

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length++
+    this.items.sort(function(a, b) {
+         return  a - b
+       })
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if(!this.items[pos]){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos]
+  }
 
-  min() {}
 
-  sum() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items)
+  }
+
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items)
+  }
+  
+   
+
+  sum() {   let sum = 0;
+    for (let i = 0;i<this.items.length;i++) {
+      sum += this.items[i];}
+    }
 
   avg() {}
-}
 
+}
 module.exports = SortedList;
